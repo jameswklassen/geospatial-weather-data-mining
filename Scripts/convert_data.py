a8ce152f0c52251119a0067a32af4894fb99ff3e
piboky <75762419+piboky@users.noreply.github.com>
2020-12-09 21:24:07 -0600

    Update conversion method

--- Scripts/convert_data.py
+++ Scripts/convert_data.py
@@ -3,26 +3,36 @@ import numpy as np
 import json
 import multiprocessing as mp
 
-KELVIN = 271
+KELVIN = 273.15
 TOTAL_LAT = 180
 TOTAL_LON = 360
 NUM_SLICES = 4
 SLICE_SIZE = 0.25
 
+variables = [
+    'ten_metre_U_wind_component',
+    'ten_metre_V_wind_component',
+    'two_metre_temperature',
+    'mean_sea_level_pressure',
+    'mean_wave_direction',
+    'sea_surface_temperature',
+    'total_cloud_cover'
+]
+
 def mean(var, lat, lon, howmany):
     mean = np.mean(var[0,lat:lat+howmany, lon:lon+howmany])
 
     # Workaround for masked values - might need a better solution in the future
-    return -32767 if np.ma.is_masked(mean) else mean
+    return None if np.ma.is_masked(mean) else mean
 
 def process_data(lat_start, lat_end):
     print(f"start: {lat_start} end: {lat_end}")
-    coordinates = [['-']*TOTAL_LON for _ in range(TOTAL_LAT)]
+    
+    coordinates = {variables[i]:[['-']*TOTAL_LON for _ in range(TOTAL_LAT)] for i in range(len(variables))}
 
     filename = 'EAR5-01-01-2020.nc'
     ctd = Dataset(filename, 'r')
 
-    land_sea_mask = ctd.variables['lsm']
     ten_metre_U_wind_component = ctd.variables['u10']
     ten_metre_V_wind_component = ctd.variables['v10']
     two_metre_temperature = ctd.variables['t2m']
@@ -33,33 +43,40 @@ def process_data(lat_start, lat_end):
 
     for curr_lat in range(lat_start, lat_end):
         for curr_lon in range(TOTAL_LON):
-            coordinates[curr_lat][curr_lon] = {
-                'lat': curr_lat,
-                'lon': curr_lon,
-                'land_sea_mask': mean(land_sea_mask, curr_lat, curr_lon, NUM_SLICES),
-                'ten_metre_U_wind_component': mean(ten_metre_U_wind_component, curr_lat, curr_lon, NUM_SLICES),
-                'ten_metre_V_wind_component': mean(ten_metre_V_wind_component, curr_lat, curr_lon, NUM_SLICES),
-                'two_metre_temperature': mean(two_metre_temperature, curr_lat, curr_lon, NUM_SLICES),
-                'mean_sea_level_pressure': mean(mean_sea_level_pressure, curr_lat, curr_lon, NUM_SLICES),
-                'mean_wave_direction': mean(mean_wave_direction, curr_lat, curr_lon, NUM_SLICES),
-                'sea_surface_temperature': mean(sea_surface_temperature, curr_lat, curr_lon, NUM_SLICES),
-                'total_cloud_cover': mean(total_cloud_cover, curr_lat, curr_lon, NUM_SLICES),
-            }
+            x = mean(sea_surface_temperature, curr_lat, curr_lon, NUM_SLICES)
+            if x is None:
+                coordinates['ten_metre_U_wind_component'][curr_lat][curr_lon] = None
+                coordinates['ten_metre_V_wind_component'][curr_lat][curr_lon] = None
+                coordinates['two_metre_temperature'][curr_lat][curr_lon] = None
+                coordinates['mean_sea_level_pressure'][curr_lat][curr_lon] = None
+                coordinates['mean_wave_direction'][curr_lat][curr_lon] = None
+                coordinates['sea_surface_temperature'][curr_lat][curr_lon] = None
+                coordinates['total_cloud_cover'][curr_lat][curr_lon] = None
+            else:
+                coordinates['ten_metre_U_wind_component'][curr_lat][curr_lon] = mean(ten_metre_U_wind_component, curr_lat, curr_lon, NUM_SLICES)
+                coordinates['ten_metre_V_wind_component'][curr_lat][curr_lon] = mean(ten_metre_V_wind_component, curr_lat, curr_lon, NUM_SLICES)
+                coordinates['two_metre_temperature'][curr_lat][curr_lon] = mean(two_metre_temperature, curr_lat, curr_lon, NUM_SLICES) - KELVIN
+                coordinates['mean_sea_level_pressure'][curr_lat][curr_lon] = mean(mean_sea_level_pressure, curr_lat, curr_lon, NUM_SLICES) / 1000
+                coordinates['mean_wave_direction'][curr_lat][curr_lon] = mean(mean_wave_direction, curr_lat, curr_lon, NUM_SLICES)
+                coordinates['sea_surface_temperature'][curr_lat][curr_lon] = x - KELVIN
+                coordinates['total_cloud_cover'][curr_lat][curr_lon] = mean(total_cloud_cover, curr_lat, curr_lon, NUM_SLICES)
+
     return coordinates
 
 pool = mp.Pool(mp.cpu_count())
 
 # Pool.starmap is multithreading magic
-results = pool.starmap(process_data, [(i, i+1) for i in range(TOTAL_LAT)])
+results = pool.starmap(process_data, [(i, i+10) for i in range(0,TOTAL_LAT,10)])
 
-coordinates = [['-']*TOTAL_LON for _ in range(TOTAL_LAT)]
+coordinates = {variables[i]:[['-']*TOTAL_LON for _ in range(TOTAL_LAT)] for i in range(len(variables))}
 
 # Merge all the partial results and we're done
 for result in results:
-    for i in range(TOTAL_LAT):
-        for j in range(TOTAL_LON):
-            if result[i][j] != '-':
-                coordinates[i][j] = result[i][j]
+    for var in variables:
+        for i in range(TOTAL_LAT):
+            for j in range(TOTAL_LON):
+                if result[var][i][j] != '-':
+                    coordinates[var][i][j] = result[var][i][j]
 
 # Save it once we're done processing
 with open('converted_data.json', 'w') as outfile:
